485cc11b27985dd965bbe46d591953c6419d0fc8
Renato Alves Felix <renatofelix.redhat@gmail.com>
2021-03-17 14:38:10 -0300

    wip

--- kkk.sh
+++ kkk.sh
@@ -2,7 +2,7 @@
 
 THIS_SCRIPT=$(basename -- "$0")
 echo Executando $THIS_SCRIPT
-#set -e
+
 # check nodes unscheduled
 NODES_UNSCHEDULABLE_SIZE=$(oc get nodes --field-selector spec.unschedulable=true | awk 'FNR > 1  {print}' | wc -l | xargs)
 
@@ -13,6 +13,15 @@ if [[ ${NODES_UNSCHEDULABLE_SIZE} != '0' ]]; then
   echo "${HEADER%;*}\n${BODY#*;}\n"
 fi
 
+# check nodes NOT (unscheduled and ready)
+NODES_NOT_SCHEDULED_OR_READY_SIZE=$(oc get nodes | awk 'FNR > 1 {if ($2!="Ready" && $2 !~ /SchedulingDisabled/)  print}' | wc -l | xargs)
+
+if [[ ${NODES_NOT_SCHEDULED_OR_READY_SIZE} != '0' ]]; then
+  TITLE="[NODES_NOT_SCHEDULED_OR_READY_SIZE] : ${NODES_NOT_SCHEDULED_OR_READY_SIZE}"
+  HEADER="=======\n${TITLE}\n-------;----------"
+  BODY=$(oc get nodes | awk 'FNR > 1 {if ($2!="Ready" && $2 !~ /SchedulingDisabled/)  print}')
+  echo "${HEADER%;*}\n${BODY#*;}\n"
+fi
 
 ## Explorar status dos nodes
 #tail nodes <> not Ready
